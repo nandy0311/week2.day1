87089d4bbb57d4d1bd49413fe9af9f2b3d7c2878
nandy0311 <107383470+nandy0311@users.noreply.github.com>
2022-06-21 17:27:56 +0530

    Update ReverseEvenWords.java

--- ReverseEvenWords.java
+++ ReverseEvenWords.java
@@ -4,7 +4,7 @@ public class ReverseEvenWords {
 	//Build a logic to reverse the even position words (output: I ma a erawtfos tester)
 	public static void main(String[] args) {
 		String test= "I am a software tester" ;
-		//			  0 1  2 3         4
+		//index	      0 1  2 3         4
 		String [] array = test.split(" ");
 		for (int i=0; i< array.length;i++) {
 			if(i %2 == 0) {
